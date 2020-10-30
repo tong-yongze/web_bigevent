9a9536b50e9d58c4d4cc0575ff43ebf6220a4356
童永泽 <604232797@qq.com>
2020-10-30 15:04:20 +0800

    04_为注册表单进行校验

--- assets/js/login.js
+++ assets/js/login.js
@@ -18,7 +18,16 @@ $(function () {
         pwd: [
             /^[\S]{6,12}$/
             , '密码必须6到12位，且不能出现空格'
-        ]
+        ],
+        repwd: function (value) {
+            // 通过形参拿到确认密码框内的内容
+            // 需要拿到密码框的内容
+            // 进行一次判断 如果不一致就返回提示
+            var pwd = $('.reg-box[name=password]').val();
+            if (pwd !== value) {
+                return '两次输入不一致'
+            }
+        }
     }
     )
 })
