0e2b7fd8713ecf8dfc1a0da9e8ace690bfc8be94
童永泽 <604232797@qq.com>
2020-10-30 14:39:01 +0800

    03_登录表单的规则校验

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,5 +10,15 @@ $(function () {
         // 隐藏注册框
         $('.login-box').show();
         $('.reg-box').hide();
-    })
+    });
+
+    // 自定义校验规则
+    var form = layui.form;
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ]
+    }
+    )
 })
