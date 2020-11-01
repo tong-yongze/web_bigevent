c0b0a62a8395a39e48509dbc589fd416acc18511
童永泽 <604232797@qq.com>
2020-11-01 17:55:14 +0800

    修复提交修改bug

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -25,5 +25,27 @@ $(function () {
         })
     }
 
+    $('#btnReset').on('click', function (e) {
+        // 阻止表单的默认重置行为
+        e.preventDefault()
+        // 重新获取
+        initUserInfo()
+    });
 
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('修改用户信息失败！')
+                }
+                layer.msg('修改用户信息成功！')
+                // 调用父页面中的方法 重新渲染用户的头像和信息
+                window.parent.getUserInfo()
+            }
+        })
+    })
 })
