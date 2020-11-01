747ef76f0dbcd68e477a0990b240b427f3aa9bbb
童永泽 <604232797@qq.com>
2020-11-01 14:27:19 +0800

    08_请求拦截器

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,9 +4,19 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+
     if (options.url.includes('/my/'))
         options.headers = {
             Authorization: localStorage.getItem('token') || ''
-
         }
+
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 1.清空 token
+            localStorage.removeItem('token');
+            // 2 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
+
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,18 @@ function getUserInfo() {
                 return layui.layer.msg('获取用户信息失败!');
             }
             renderAvatat(res.data)
-        }
+        },
+
+        // complete: function (res) {
+        //     // console.log(res);
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+        //         // 1.清空 token
+        //         localStorage.removeItem('token');
+        //         // 2 强制跳转到登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
+
     })
 }
 
