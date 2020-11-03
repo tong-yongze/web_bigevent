548e669284fce7df438d6cda8c2c413289b103db
童永泽 <604232797@qq.com>
2020-11-03 19:49:07 +0800

    选择封面完成

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -2,6 +2,8 @@ $(function () {
     var layer = layui.layer
     var form = layui.form
 
+    // 初始化富文本编辑器
+    initEditor()
     initCate()
     function initCate() {
         $.ajax({
@@ -11,7 +13,6 @@ $(function () {
                 if (res.status !== 0) {
                     return layer.msg('获取文章分类列表失败！')
                 }
-                layer.msg('获取文章分类列表成功！')
 
                 // 调用模板引擎  渲染分类下拉菜单
                 var htmlStr = template('tpl-cate', res)
@@ -25,4 +26,40 @@ $(function () {
 
     }
 
+
+    // 1. 初始化图片裁剪器
+    var $image = $('#image')
+
+    // 2. 裁剪选项
+    var options = {
+        aspectRatio: 400 / 280,
+        preview: '.img-preview'
+    }
+
+    // 3. 初始化裁剪区域
+    $image.cropper(options)
+
+
+    // 为选择封面框按钮 绑定点击事件 处理函数
+    $('#btnChooseImage').on('click', function () {
+        $('#coverFile').click()
+    })
+
+    // 监听coverFile  的 change 事件  获取用户选择的文件列表
+    $('#coverFile').on('change', function (e) {
+        // 获取到文件的列表数组
+        var files = e.target.files
+        // 判断用户是否选择了文件
+        if (files.length === 0) {
+            return
+        }
+        // 根据文件 创建对应的 URL 地址
+        var newImgURL = URL.createObjectURL(files[0])
+
+        // 为裁剪区域重新设置图片
+        $image
+            .cropper('destroy')      // 销毁旧的裁剪区域
+            .attr('src', newImgURL)  // 重新设置图片路径
+            .cropper(options)        // 重新初始化裁剪区域
+    })
 })
